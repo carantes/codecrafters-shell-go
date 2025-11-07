cd599205e152030bf5322e19f08c4e2d7df2a921
Carlos Ferreira <carantes.ferreira@gmail.com>
2025-11-07 13:23:43 +0100

    Handle invalid commands

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -10,5 +11,17 @@ var _ = fmt.Fprint
 var _ = os.Stdout
 
 func main() {
+	// Start a shell prompt
 	_, _ = fmt.Fprint(os.Stdout, "$ ")
+
+	//	Read user input
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	//	Print command not found message
+	fmt.Println(cmd[:len(cmd)-1] + ": command not found")
 }
