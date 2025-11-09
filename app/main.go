8f82f5611189e2be5d54402a1bb80ff8b5d7979c
Carlos Ferreira <carantes.ferreira@gmail.com>
2025-11-09 22:56:22 +0100

    Type find files in path

--- app/main.go
+++ app/main.go
@@ -12,6 +12,8 @@ import (
 var _ = fmt.Fprint
 var _ = os.Stdout
 
+var builtInCommands = []string{"exit", "echo", "type"}
+
 func main() {
 
 	for {
@@ -30,27 +32,70 @@ func main() {
 			continue
 		}
 
-		cmd := strings.Fields(input)[0]
-		args := strings.Fields(input)[1:]
-
-		shellCommands := []string{"exit", "echo", "type"}
+		argv := strings.Fields(input)
+		cmd := argv[0]
 
 		switch cmd {
 		case "exit":
-			os.Exit(0)
+			exitCommand(argv)
 		case "echo":
-			if len(args) > 0 {
-				fmt.Println(strings.Join(args, " "))
-			}
+			echoCommand(argv)
 		case "type":
-			if len(args) > 0 && slices.Contains(shellCommands, args[0]) {
-				fmt.Println(args[0] + " is a shell builtin")
-			} else {
-				fmt.Println(args[0] + ": not found")
-			}
+			typeCommand(argv)
 		default:
 			fmt.Println(cmd + ": command not found")
 		}
 
 	}
+}
+
+func exitCommand(_ []string) {
+	code := 0
+	os.Exit(code)
+}
+
+func echoCommand(argv []string) {
+	if len(argv) > 1 {
+		fmt.Println(strings.Join(argv[1:], " "))
+	} else {
+		fmt.Println()
+	}
+}
+
+func typeCommand(argv []string) {
+	// Not enough arguments
+	if len(argv) < 2 {
+		return
+	}
+
+	arg := argv[1]
+
+	// Check if the command is a shell builtin
+	if slices.Contains(builtInCommands, arg) {
+		fmt.Println(arg + " is a shell builtin")
+		return
+	}
+
+	// Find file in PATH
+	if file, exists := findFileInPath(arg); exists {
+		fmt.Fprintf(os.Stdout, "%s is %s\n", arg, file)
+		return
+	}
+
+	// Command not found
+	fmt.Println(arg + ": not found")
+}
+
+func findFileInPath(command string) (string, bool) {
+	pathEnv := os.Getenv("PATH")
+	paths := strings.Split(pathEnv, string(os.PathListSeparator))
+
+	for _, dir := range paths {
+		fullPath := dir + "/" + command
+		if fileInfo, err := os.Stat(fullPath); err == nil && !fileInfo.IsDir() {
+			return fullPath, true
+		}
+	}
+
+	return "", false
 }
